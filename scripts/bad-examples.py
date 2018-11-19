b4a4f346810736c04df4db180ef20cbedaeea961
John P. McCrae <john@mccr.ae>
2018-11-19 15:14:51 +0000

    Add bad examples for Issue 30

---
+++ scripts/bad-examples.py
@@ -0,0 +1,28 @@
+import xml.etree.ElementTree as ET
+from nltk.metrics import edit_distance
+tree = ET.parse('wn31.xml')
+
+synsets = {}
+
+for entry in tree.findall(".//LexicalEntry"):
+    lemma = entry.find("Lemma").attrib["writtenForm"]
+    if lemma.endswith(")"):
+        lemma = lemma[:-3]
+    lemmas = [lemma]
+    for form in entry.findall("Form"):
+        lemmas.append(form.attrib["writtenForm"])
+    for sense in entry.findall("Sense"):
+        synset = sense.attrib["synset"]
+        if synset not in synsets:
+            synsets[synset] = lemmas
+        else:
+            synsets[synset].extend(lemmas)
+
+def is_likely_in(example, lemma):
+    return edit_distance(example, lemma) - len(example) + len(lemma) - 2 < 0
+
+for synset in tree.findall(".//Synset"):
+    synset_id = synset.attrib["id"]
+    for example in synset.findall("Example"):
+        if not any(is_likely_in(example.text, lemma) for lemma in synsets[synset_id]):
+            print("%s,%s,%s" % (synset_id,example.text,str(synsets[synset_id])))
